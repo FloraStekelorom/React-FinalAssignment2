1565249d5bf212b9930c46ef221fcf2606a72ec1
Flora <flora.e.hewitt@gmail.com>
2020-02-08 13:55:26 +0400

    added the navbar and footer and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put our custom imports below
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
